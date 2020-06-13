50d4e4416984f16f0b1156bb9f870fe8a8d4a5e2
isikosoy <isik.ozsoy@ug.bilkent.edu.tr>
2020-06-13 15:37:56 +0300

    Views, layouts were implemented.

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mylayout);
+        final TextView calculatorScreen = findViewById(R.id.calculatorScreen);
+        final Button n0 = findViewById(R.id.n0);
+        final Button n1 = findViewById(R.id.n1);
+        final Button n2 = findViewById(R.id.n2);
+        final Button n3 = findViewById(R.id.n3);
+        final Button n4 = findViewById(R.id.n4);
+        final Button n5 = findViewById(R.id.n5);
+        final Button n6 = findViewById(R.id.n6);
+        final Button n7 = findViewById(R.id.n7);
+        final Button n08 = findViewById(R.id.n8);
+        final Button n9 = findViewById(R.id.n9);
+        final Button addition = findViewById(R.id.addition);
+        final Button subtraction = findViewById(R.id.subtraction);
+
+    }
+}
